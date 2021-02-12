7197ca996d41dc26a5a57ba6d5430a0448185605
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-02-12 22:57:59 +0530

    Create header.ejs

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
-const mongoose = require('mongoose');
+import express from 'express';
+import bodyParser from 'body-parser';
+import ejs from 'ejs';
+import mongoose from 'mongoose';
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render("home");
+});
+
+app.listen(8081, () => {console.log("Server started");});
