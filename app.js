b390f00db35b1564317e906f09ae50f7489ccb8d
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-02-13 01:18:28 +0530

    Setup mongodb local connection and create mongoose meme schema and model

--- app.js
+++ app.js
@@ -9,6 +9,29 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
+// Connect MongoDB at default port 27017.
+mongoose.connect('mongodb://localhost:27017/xmemeDB', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}, (err) => {
+    if (!err) {
+        console.log('MongoDB Connection Succeeded.')
+    } else {
+        console.log('Error in DB connection: ' + err)
+    }
+});
+
+// Create memeSchema
+const memeSchema = new mongoose.Schema({
+    name: String,
+    caption: String,
+    url: String
+});
+
+// Create meme model 
+const Meme = mongoose.model('Meme', memeSchema);
+
 app.get('/', (req, res) => {
     res.render("home");
 });
