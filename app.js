3e465f2a3af2184dfed4f429adcdd4517dc050f2
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-02-15 00:33:48 +0530

    Refactor

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import mongoose from 'mongoose';
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
@@ -62,4 +64,4 @@ app.post('/', (req, res) => {
     });
 });
 
-app.listen(8081, () => {console.log("Server started");});
+app.listen(port, () => {console.log("Server started");});
